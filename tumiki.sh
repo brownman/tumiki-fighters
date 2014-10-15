ab13df3cf71a9f69c9a13d3a60eebd620fdcf000
brownman <brownman556@gmail.com>
2014-10-15 11:19:22 +0300

    Update tumiki.sh

--- tumiki.sh
+++ tumiki.sh
@@ -1,3 +1,6 @@
+#https://www.google.co.il/url?sa=t&rct=j&q=&esrc=s&source=web&cd=2&cad=rja&uact=8&ved=0CCYQFjAB&url=https%3A%2F%2Fwiki.archlinux.org%2Findex.php%2FPacman_Rosetta&ei=9y0-VMmuAc3yaNipgPgB&usg=AFQjCNEh0yeRzkBcbT1jxdwPoBo0v_GRHA&sig2=_gRDlOnxkSBFctOfbEgSlQ&bvm=bv.77412846,d.d2s
+
+sudo apt-get build-dep tumiki-fighters
 echo hello from tumiki
 #print_g gay tumiki
 git clone --depth=1 https://github.com/brownman/tumiki-fighters --branch=game
