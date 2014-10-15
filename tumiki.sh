65383796df8a39074aafe28304056b4b60580e1b
brownman <brownman556@gmail.com>
2014-10-15 11:16:21 +0300

    fix typo

--- tumiki.sh
+++ tumiki.sh
@@ -1,5 +1,5 @@
 echo hello from tumiki
 #print_g gay tumiki
-git clont --depth=1 https://github.com/brownman/tumiki-fighters --branch=game
+git clone --depth=1 https://github.com/brownman/tumiki-fighters --branch=game
 cd tumiki*
 make
